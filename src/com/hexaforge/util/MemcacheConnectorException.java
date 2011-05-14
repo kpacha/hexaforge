2ffb9915499f12851d36eac3f1ae9e88c508ed4e
kpacha666@gmail.com <kpacha666@gmail.com@9a2bed3a-4661-2173-d4ae-9a3fe8f4e174>
2011-05-14 11:19:09 +0000

    Incorporación de interfaz al servicio de Memcache para evitar la obligación de purgar manualmente la cache en los despliegues

---
+++ src/com/hexaforge/util/MemcacheConnectorException.java
@@ -0,0 +1,15 @@
+package com.hexaforge.util;
+
+@SuppressWarnings("serial")
+public class MemcacheConnectorException extends Exception
+{
+        MemcacheConnectorException( String msg )
+        {
+                super( msg );
+        }
+        
+        MemcacheConnectorException( Exception e )
+        {
+                super( e );
+        }
+}
