75f9e2ea981a3d0500a6386e5f556783cc600c17
kpacha666@gmail.com <kpacha666@gmail.com@9a2bed3a-4661-2173-d4ae-9a3fe8f4e174>
2012-06-02 11:46:25 +0000

    Corregido error que permitía a los jugadores atacarse a sí mismos

--- src/com/hexaforge/core/cell/Piece.java
+++ src/com/hexaforge/core/cell/Piece.java
@@ -5,43 +5,43 @@ import com.hexaforge.util.RandomGenerator;
 
 abstract public class Piece extends AbstractCell implements CellInterface {
 
-	public Piece(int xPos, int yPos, int owner) {
-		setX(xPos);
-		setY(yPos);
-		setOwner(owner);
-		this.range = 1;
-	}
+    public Piece(int xPos, int yPos, int owner) {
+	setX(xPos);
+	setY(yPos);
+	setOwner(owner);
+	this.range = 1;
+    }
 
-	/**
-	 * @return the owner
-	 */
-	public int getOwner() {
-		return this.owner;
-	}
+    /**
+     * @return the owner
+     */
+    public int getOwner() {
+	return this.owner;
+    }
 
-	/**
-	 * @param owner
-	 *            the owner to set
-	 */
-	public void setOwner(int owner) {
-		this.owner = owner;
-	}
+    /**
+     * @param owner
+     *            the owner to set
+     */
+    public void setOwner(int owner) {
+	this.owner = owner;
+    }
 
-	protected boolean doMove(int x, int y) {
-		this.setX(x);
-		this.setY(y);
-		return true;
-	}
+    protected boolean doMove(int x, int y) {
+	this.setX(x);
+	this.setY(y);
+	return true;
+    }
 
-	public boolean attack(CellInterface target) {
-		boolean success = false;
-		if (!target.getCode().isObstacle())
-			success = doAttack(target);
-		return success;
-	}
+    public boolean attack(CellInterface target) {
+	boolean success = false;
+	if (!target.getCode().isObstacle() && target.getOwner() != owner)
+	    success = doAttack(target);
+	return success;
+    }
 
-	protected boolean doAttack(CellInterface cell) {
-		return RandomGenerator.getInstance().nextBoolean();
-	}
+    protected boolean doAttack(CellInterface cell) {
+	return RandomGenerator.getInstance().nextBoolean();
+    }
 
 }
